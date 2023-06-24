af35bc9ba9361fc3856461ec03f435f80e15f063
adi <example@example.com>
2023-06-24 09:56:48 +0700

    inial committed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use App\Controllers\SalonController;
+
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
@@ -28,61 +30,81 @@ $routes->setAutoRoute(true);
  * --------------------------------------------------------------------
  */
 
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
-$routes->get('/', 'todolist::index');
-$routes->get('ktp', 'ktp::index');
-$routes->get('/', 'Pesan::index');
+// // We get a performance increase by specifying the default
+// // route since we don't have to scan directories.
+// $routes->get('/', 'todolist::index');
+// $routes->get('ktp', 'ktp::index');
+// $routes->get('/', 'Pesan::index');
 
-use App\Controllers\AsistenController;
+// use App\Controllers\AsistenController;
 
-$routes->get('/asisten', 'AsistenController::index');
-$routes->get('/asisten/logout', 'AsistenController::logout');
-$routes->get('/asisten/loginpages', 'AsistenController::login');
-$routes->match(
-    ['get', 'post'],
-    'asisten/simpan',
-    [AsistenController::class, 'simpan']
-);
-$routes->match(
-    ['get', 'post'],
-    'asisten/update',
-    [AsistenController::class, 'update']
-);
-$routes->match(
-    ['get', 'post'],
-    'asisten/delete',
-    [AsistenController::class, 'delete']
-);
-$routes->match(
-    ['get', 'post'],
-    'asisten/search',
-    [AsistenController::class, 'search']
-);
-$routes->match(
-    ['get', 'post'],
-    'asisten/check',
-    [AsistenController::class, 'check']
-);
+// $routes->get('/asisten', 'AsistenController::index');
+// $routes->get('/asisten/logout', 'AsistenController::logout');
+// $routes->get('/asisten/loginpages', 'AsistenController::login');
+// $routes->match(
+//     ['get', 'post'],
+//     'asisten/simpan',
+//     [AsistenController::class, 'simpan']
+// );
+// $routes->match(
+//     ['get', 'post'],
+//     'asisten/update',
+//     [AsistenController::class, 'update']
+// );
+// $routes->match(
+//     ['get', 'post'],
+//     'asisten/delete',
+//     [AsistenController::class, 'delete']
+// );
+// $routes->match(
+//     ['get', 'post'],
+//     'asisten/search',
+//     [AsistenController::class, 'search']
+// );
+// $routes->match(
+//     ['get', 'post'],
+//     'asisten/check',
+//     [AsistenController::class, 'check']
+// );
 
 
 
+// // use App\Controllers\Pages;
+
+// // $routes->get('pages', [Pages::class, 'index']);
+// // $routes->get('(:segment)', [Pages::class, 'view']);
+
+// use App\Controllers\News;
 // use App\Controllers\Pages;
 
+// $routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
+// $routes->get('news/(:segment)', [News::class, 'view']);
+// $routes->get('news', [News::class, 'index']);
 // $routes->get('pages', [Pages::class, 'index']);
 // $routes->get('(:segment)', [Pages::class, 'view']);
 
-use App\Controllers\News;
-use App\Controllers\Pages;
 
-$routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
-$routes->get('news/(:segment)', [News::class, 'view']);
-$routes->get('news', [News::class, 'index']);
-$routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
+// =========== SALON CONTROLLER ===============
+$routes->get('/start', 'SalonController::salonStart');
+$routes->get('/home', 'SalonController::index');
+$routes->get('/homeL', 'SalonController::afterLogin');
+$routes->get('/homeAdmin', 'SalonController::homeAdmin');
+$routes->get('/salon/logout', 'SalonController::logout');
 
-
-
+$routes->match(['get', 'post'], 'salon/salonLogin', [SalonController::class, 'login']);
+$routes->match(['get', 'post'], 'salon/salonCheckLogin', [SalonController::class, 'checkLogin']);
+$routes->match(['get', 'post'], 'salon/salonLoginAdmin', [SalonController::class, 'loginAdmin']);
+$routes->match(['get', 'post'], 'salon/salonCheckLoginAdmin', [SalonController::class, 'checkLoginAdmin']);
+$routes->match(['get', 'post'], 'salon/salonRegister', [SalonController::class, 'register']);
+$routes->match(['get', 'post'], 'salon/salonCheckRegister', [SalonController::class, 'checkRegister']);
+$routes->match(['get', 'post'], 'salon/salonPricelist', [SalonController::class, 'list']);
+$routes->match(['get', 'post'], 'salon/salonPriceAdmin', [SalonController::class, 'listPriceAdmin']);
+$routes->match(['get', 'post'], 'salon/salonPricelistL', [SalonController::class, 'listLogin']);
+$routes->match(['get', 'post'], 'salon/salonTambahJasa', [SalonController::class, 'tambahJasa']);
+$routes->match(['get', 'post'], 'salon/salonHapusJasa', [SalonController::class, 'hapusJasa']);
+$routes->match(['get', 'post'], 'salon/simpanReservasi', [SalonController::class, 'simpanRev']);
+$routes->match(['get', 'post'], '/salon/salonValidasiPembayaran', [SalonController::class, 'ValidasiPembayaran']);
+$routes->match(['get', 'post'], '/salon/updateValidasi', [SalonController::class, 'updateValidasi']);
 
 /*
  * --------------------------------------------------------------------
